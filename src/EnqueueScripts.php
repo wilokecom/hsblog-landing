2dc5da3a7ff61f0951d7acbe3b5745ec8e34fa60
nguyen_dttn <contact.wilcity@gmail.com>
2020-03-02 15:26:25 +0700

    Resolved conflict css issue

--- src/EnqueueScripts.php
+++ src/EnqueueScripts.php
@@ -15,8 +15,10 @@ class EnqueueScripts
     
     public function register()
     {
-        wp_enqueue_script('hsbloglanding-scripts', HSBLOG_LANDING_JS_URL . 'scripts.js', true, HSBLOG_LANDING_VERSION);
-        wp_enqueue_style('hsbloglanding-style', HSBLOG_LANDING_CSS_URL . 'styles.css', false, HSBLOG_LANDING_VERSION);
-        wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', false, HSBLOG_LANDING_VERSION);
+        if (is_page_template('templates/hsblog-landing.php')) {
+            wp_enqueue_script('hsbloglanding-scripts', HSBLOG_LANDING_JS_URL . 'scripts.js', true, HSBLOG_LANDING_VERSION);
+            wp_enqueue_style('hsbloglanding-style', HSBLOG_LANDING_CSS_URL . 'styles.css', false, HSBLOG_LANDING_VERSION);
+            wp_enqueue_style('bootstrap', 'https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css', false, HSBLOG_LANDING_VERSION);
+        }
     }
 }
