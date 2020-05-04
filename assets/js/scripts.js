23be4ca60990d9822f17fddd0913a63a3b7adcf9
lemanh-tuong <55147271+lemanh-tuong@users.noreply.github.com>
2020-05-04 20:03:48 +0700

    Update scripts.js

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -48,128 +48,13 @@
 
       collapseButton.addEventListener("click", toggleMenu());
     }
-// PopUp
-    const videoPopUps = [...document.querySelectorAll(".video-pop-up")];
-
-    /**
-     *
-     * @param {string} tagName
-     * @param {string} className
-     * @return HTMLElement
-     * @example
-     * ```
-     * const button = createElement("button", "close-btn")
-     * ```
-     */
-
-    function createElement(tagName, className = '') {
-      const node = document.createElement(tagName);
-      node.setAttribute("class", className);
-      return node;
-    }
-
-    /**
-     *
-     * @param {{className: string, src: string}} param0
-     * @return HTMLElement
-     * @example
-     * ```
-     *  const video = createIframeVideo({className: "video", src: "youtube.com"})
-     * ```
-     */
-
-    function createIframeVideo({className, src}) {
-      const video = createElement("iframe", className);
-      video.setAttribute("src", src);
-      return video;
-    }
-
-    /**
-     *
-     * @param {{buttonNode: HTMLElement, videoNode: HTMLElement}} param0
-     * @return HTMLElement
-     * @example
-     * ```
-     *  const button = createElement("button", "buttonClassName")
-     *  const video = createIframeVideo({className: "video", src: "youtube.com"})
-     *  const popup = createPopIp({buttonNode: button, videoNode: video})
-     * ```
-     */
-
-    function createPopUp({buttonNode, videoNode}) {
-      const popUp = createElement("div", "pop-up");
-      const popUpContainer = createElement("div", "pop-up__container");
-      const popUpContent = createElement("div", "embed-responsive embed-responsive-16by9");
-      const closeButton = buttonNode;
-      const video = videoNode;
-      popUpContent.appendChild(video);
-      popUpContainer.appendChild(popUpContent);
-      popUp.appendChild(closeButton);
-      popUp.appendChild(popUpContainer);
-      return popUp;
-    }
-
-    class VideoPopup {
-      constructor(src, options) {
-        const defaultOptions = {
-          autoplay: true
-        };
-        this.options = Object.assign({}, options, defaultOptions);
-        this.closeBtn = createElement("button", "close-btn");
-        if (this.options.autoplay) {
-          this.video = createIframeVideo({className: "embed-responsive-item", src: `${src}?&autoplay=1`});
-        }
-
-        this.popup = createPopUp({buttonNode: this.closeBtn, videoNode: this.video});
-        this.closePopUp = this.closePopUp.bind(this);
-        this.showPopUp = this.showPopUp.bind(this);
-      }
-
-      closePopUp(e) {
-        e.preventDefault();
-        body.removeChild(this.popup);
-      }
-
-      showPopUp() {
-        body.appendChild(this.popup);
-        this.closeBtn.addEventListener("click", this.closePopUp);
-        this.popup.addEventListener("click", this.closePopUp);
-        window.addEventListener("keyup", (e) => {
-          if (e.key === "Escape") {
-            this.closePopUp(e);
-          }
-        });
-      }
-    }
-
-    videoPopUps.forEach(videoPopUp => videoPopUp.addEventListener("click", (e) => {
-      e.preventDefault();
-      const dataSrc = videoPopUp.getAttribute("data-src");
-      const video = new VideoPopup(dataSrc);
-      video.showPopUp();
-    }));
-
 
     // Section Demo
 
-    const backgroundUrl = {
-      musicdemo: 'https://images.pexels.com/photos/3683056/pexels-photo-3683056.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/3683056/pexels-photo-3683056.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500 2',
-      fashiondemo: 'https://images.pexels.com/photos/4108271/pexels-photo-4108271.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/4108271/pexels-photo-4108271.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500 2x',
-      bueatydemo: 'https://images.pexels.com/photos/3989816/pexels-photo-3989816.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/3989816/pexels-photo-3989816.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500 2x',
-      fooddemo: 'https://images.pexels.com/photos/1713953/pexels-photo-1713953.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/1713953/pexels-photo-1713953.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500 2x',
-      techdemo: 'https://images.pexels.com/photos/3024995/pexels-photo-3024995.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500 1x, https://images.pexels.com/photos/3024995/pexels-photo-3024995.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500 2x',
-    }
-
-    const sectionDemos = document.querySelector('.section-demos');
-    const buttonsFilter = sectionDemos ? [...sectionDemos.querySelectorAll('.wil-btn')] : undefined;
-    if(!!buttonsFilter) {
-      buttonsFilter.forEach(btn => {
-        const attr = btn.getAttribute('data-filter');
-        btn.addEventListener('click', () => {
-          sectionDemos.style.backgroundImage = `url('${backgroundUrl[attr.slice(1, attr.length)]}')`
-        })
-      })    
-    }
+    jQuery('.section-demos').find('.wil-btn').click(function() {
+        const attrBg = jQuery(this).data('bg');
+        jQuery('.section-demos').css('background-image', `url('${attrBg}')`);
+    })
 
 
 
